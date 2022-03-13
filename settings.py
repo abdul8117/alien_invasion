7ae535a8f80ca9361d75272b76dbdfb72f8e038f
Abdul <abdulahad2004@yahoo.com>
2022-03-13 22:29:42 +0300

    Main class

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """A class to store all settings for the Alien Invasion game."""
+
+    def __init__(self):
+        """Initialise the game's settings."""
+
+        # Screen settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
