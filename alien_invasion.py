e29dd0614b814e2de6ddc171d64c5ba1288eeabe
Abdul <abdulahad2004@yahoo.com>
2022-03-23 21:41:57 +0300

    Ship movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,6 +26,8 @@ class AlienInvasion:
             # Watch for keyboard and mouse events. 
             self._check_events()
 
+            self.ship.update()
+
             # redrew the screen suring each pass through the loop
             # Make the most recently drawn screen visible
             self._update_screen()
@@ -35,6 +37,26 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = False
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
